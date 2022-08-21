d772cc99b134d2e9709413997301f1cbce90666a
Kshitij Tiwari <kshitiztewari098@gmail.com>
2022-08-21 16:10:13 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Travel Advisor
+An app that fetch restaurants, hotels or attractions based on the location and data filtering based on rating
+
+
+![screenshot_of_travel_advisor_app](https://i.ibb.co/PwYjryD/travel-advisor.png)
+![screenshot_of_travel_advisor_app_light_mode](https://i.ibb.co/fGh1QXM/travel-advisor-white-mode.png)
+App based on this [Youtube](https://www.youtube.com/watch?v=UKdQjQX1Pko) video. But changed the map to use mapbox api instead of google map api and added dark/light mode.
+
+## Built with
+- [React](https://reactjs.org/)
+- [React Hooks](https://reactjs.org/docs/hooks-intro.html)
+- [Map Box](https://www.mapbox.com/)
+- [REACT-MAP-GL](https://visgl.github.io/react-map-gl/)
+- [Rapid API](https://rapidapi.com/hub)
+- [Travel Advisor API](https://rapidapi.com/apidojo/api/travel-advisor)
+
+## Setup & Usage
+1. Clone this repo and go to project directory
+2. Run `npm install` and wait installing its dependencies
+3. Configure `.env` file as shown in example `.env.example`
+4. Run `npm start` to start the application
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, Box, IconButton } from "@mui/material";
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+
+import useStyles from "./styles";
+
+const Header = ({ containerRef, mode, setMode }) => {
+    const classes = useStyles();
+
+    return (
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h5" className={classes.title}>
+                    Travel Sense
+                </Typography>
+                <Box display="flex">
+                    <Typography variant="h6" className={classes.title}>
+                        Explore new places
+                    </Typography>
+                    <div className={classes.search}>
+                        <div ref={containerRef} />
+                    </div>
+                    <div>
+                        {mode} mode
+                        <IconButton
+                            sx={{ ml: 1 }}
+                            onClick={() => setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'))}
+                            color="inherit"
+                        >
+                            {mode === 'light' ? <Brightness4Icon /> : <Brightness7Icon />}
+                        </IconButton>
+                    </div>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Header;
